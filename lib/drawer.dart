f3a6b7523601b76f7e29580ff3d3a33726ea7855
luizadelnegro <ldncf42@gmail.com>
2020-10-04 21:53:03 -0300

    Adiciona ranking básico com grid e listview

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,58 +1,46 @@
 import 'package:flutter/material.dart';
 
 final entries = [
-  {
-    'title': 'Home',
-    'routename': '/'
-  },
-  {
-    'title': 'Progress',
-    'routename': '/progress'
-  },
+  {'title': 'Home', 'routename': '/'},
+  {'title': 'Progress', 'routename': '/progress'},
+  {'title': 'Ranking', 'routename': '/ranking'},
 ];
-
 
 class SideMenu extends StatelessWidget {
   SideMenu({Key key}) : super(key: key);
 
-
-
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
-            flex: 1,
-            child: DrawerHeader(
-              child: Center( 
-                
-                child: Text('Drawer Header'),
-              ),
-              decoration: BoxDecoration(color: Theme
-                  .of(context)
-                  .primaryColor),
-            )
-          ),
+              flex: 1,
+              child: DrawerHeader(
+                child: Center(
+                  child: Text('Hero Name'),
+                ),
+                decoration:
+                    BoxDecoration(color: Theme.of(context).primaryColor),
+              )),
           Expanded(
-            flex: 4,
-            child: ListView.separated(
-              itemCount: entries.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(entries[index]['title']),
-                  onTap: () {
-                    // Update the state of the app then close the drawer
-                    Navigator.of(context).pushNamed(entries[index]['routename']);
-                  },
-                );
-              },
-              separatorBuilder: (BuildContext context,
-                  int index) => const Divider(),
-            )
-          )
+              flex: 4,
+              child: ListView.separated(
+                itemCount: entries.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(entries[index]['title']),
+                    onTap: () {
+                      // Update the state of the app then close the drawer
+                      Navigator.of(context)
+                          .pushNamed(entries[index]['routename']);
+                    },
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              ))
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'route_generator.dart';
 
