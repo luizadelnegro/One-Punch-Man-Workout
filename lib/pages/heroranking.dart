817719a51472bb04660444d5151f258e4c81379e
Lucas Damo <lucasdamo1@gmail.com>
2020-10-12 00:04:35 -0300

    Add a list of the anime's heros and ranks

--- lib/pages/heroranking.dart
+++ lib/pages/heroranking.dart
@@ -1,17 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'custom_assets/custom_barless_scaffold.dart';
 
-class HeroRanking extends StatelessWidget {
+
+class HeroRanking extends StatefulWidget {
+  @override
+  _HeroRankingState createState() => _HeroRankingState();
+}
+
+class _HeroRankingState extends State<HeroRanking> {
+
+  Stream<String> getJsonHeros() {
+    return DefaultAssetBundle.of(context).loadString("assets/data/herosrank.json");
+  }
+
   @override
   Widget build(BuildContext context) {
-    final title = 'Ranking';
-    return CustomBarlessScaffold(
-      children: [
-        ListTile( 
-          title: Text("Saitama"),
-          subtitle: Text("The best"),
-        )
-      ],
-    );
+    return StreamBuilder( 
+      stream: getJsonHeros(),
+    )
   }
 }
