2b21563d6c4acc16ef294b7365b4ed8dbe44f44b
Lucas Damo <lucasdamo1@gmail.com>
2020-10-12 22:56:35 -0300

    Change heroranking appearence
    Show image for each Class S hero
    Show Class, Rank and name inside a formatted card for each hero

--- lib/pages/heroranking.dart
+++ lib/pages/heroranking.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:one_punch_man_workout/size_config.dart';
 import 'custom_assets/custom_barless_scaffold.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 
 class HeroRanking extends StatefulWidget {
@@ -11,14 +13,78 @@ class HeroRanking extends StatefulWidget {
 
 class _HeroRankingState extends State<HeroRanking> {
 
-  Stream<String> getJsonHeros() {
-    return DefaultAssetBundle.of(context).loadString("assets/data/herosrank.json");
+  Future<List<dynamic>> getJsonHeros() async {
+    String data = await DefaultAssetBundle.of(context).loadString("assets/data/herosrank.json");
+    return json.decode(data);
+  }
+
+  int getNumOfBlocksPerRow() {
+    final one_percent_screen_size = SizeConfig.blockSizeHorizontal;
+    if (one_percent_screen_size > 3) {
+      return 2;
+    }
+    else {
+      return 1;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder( 
-      stream: getJsonHeros(),
-    )
+    return FutureBuilder( 
+      future: getJsonHeros(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if(snapshot.hasData) {
+          return Scaffold(
+            body:
+              GridView.builder(
+                itemCount: snapshot.data.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: getNumOfBlocksPerRow(),
+                  mainAxisSpacing: 4,
+                ), 
+                itemBuilder: (BuildContext context, int index) {
+                    String image_str = snapshot.data[index]["image"];
+                    return Card( 
+                      child: Stack( 
+                        children: [
+                          if(image_str != null) Container(child: Image.network(image_str, fit: BoxFit.fill,), alignment: Alignment.center,),
+                          Container(
+                            child: Column( 
+                              children: [
+                                ListTile( 
+                                  title: Text("Class " + snapshot.data[index]['class'], style: TextStyle(backgroundColor: Colors.white),),
+                                  subtitle: Text("Rank - " + snapshot.data[index]['rank'].toString(), style: TextStyle(backgroundColor: Colors.white),),
+                                ),
+                                Center( 
+                                  child: Text(snapshot.data[index]['name'], style: TextStyle(backgroundColor: Colors.white),),
+                                )
+                              ],
+                            )
+                          )
+                        ]
+                      )
+                    );
+                }
+              ),
+            
+          );
+        }
+        else {
+          //print(getJsonHeros());
+          return CustomBarlessScaffold( 
+            children: [
+              Container( 
+                height: SizeConfig.blockSizeVertical*5,
+                width: SizeConfig.blockSizeHorizontal*5,
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(
+                  
+                )
+              ),
+            ],
+          );
+        }
+      }
+    );
   }
 }
