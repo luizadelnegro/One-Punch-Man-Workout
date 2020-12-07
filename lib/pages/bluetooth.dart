7f799e96ce039ec7f4d90dd7c8c397166cbbd39e
luizadelnegro <ldncf42@gmail.com>
2020-12-07 04:13:50 -0300

    update bluetooth
    update, bluetooth now working

--- lib/pages/bluetooth.dart
+++ lib/pages/bluetooth.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:one_punch_man_workout/pages/widget.dart';
-
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'dart:math';
 
+// FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
 class BluetoothApp extends StatelessWidget {
+  // FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  //     FlutterLocalNotificationsPlugin();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -42,14 +46,14 @@ class BluetoothOffScreen extends StatelessWidget {
             Icon(
               Icons.bluetooth_disabled,
               size: 200.0,
-              color: Theme.of(context).backgroundColor,
+              color: Colors.white54,
             ),
             Text(
-              'Bluetooth Adapter is ${state != null ? state.toString().substring(15) : 'not available'}.',
+              'Bluetooth Adapter is ${state.toString().substring(15)}.',
               style: Theme.of(context)
                   .primaryTextTheme
                   .subhead
-                  .copyWith(color: Theme.of(context).backgroundColor,
+                  .copyWith(color: Colors.white),
             ),
           ],
         ),
@@ -299,293 +303,3 @@ class DeviceScreen extends StatelessWidget {
     );
   }
 }
-
-// class _BluetoothAppState extends State<BluetoothApp> {
-//   //initialize
-//   FlutterBlue flutterBlue = FlutterBlue.instance;
-
-// // Start scanning
-//   flutterBlue.startScan(timeout: Duration(seconds: 4));
-
-// // Listen to scan results
-// var subscription = flutterBlue.scanResults.listen((results) {
-//     // do something with scan results
-//     for (ScanResult r in results) {
-//         print('${r.device.name} found! rssi: ${r.rssi}');
-//     }
-// });
-
-// // Stop scanning
-// flutterBlue.stopScan();
-// // /////////////////////////////
-
-// //   // Initializing a global key, as it would help us in showing a SnackBar later
-// //   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
-// //   // Get the instance of the bluetooth
-// //   FlutterBluetoothSerial bluetooth = FlutterBluetoothSerial.instance;
-
-// //   // Define some variables, which will be required later
-// //   List<BluetoothDevice> _devicesList = [];
-// //   BluetoothDevice _device;
-// //   bool _connected = false;
-// //   bool _pressed = false;
-
-// //   @override
-// //   void initState() {
-// //     super.initState();
-// //     bluetoothConnectionState();
-// //   }
-
-// //   // We are using async callback for using await
-// //   Future<void> bluetoothConnectionState() async {
-// //     List<BluetoothDevice> devices = [];
-
-// //     // To get the list of paired devices
-// //     try {
-// //       devices = await bluetooth.getBondedDevices();
-// //     } on PlatformException {
-// //       print("Error");
-// //     }
-
-// //     // For knowing when bluetooth is connected and when disconnected
-// //     bluetooth.onStateChanged().listen((state) {
-// //       switch (state) {
-// //         case FlutterBluetoothSerial.CONNECTED:
-// //           setState(() {
-// //             _connected = true;
-// //             _pressed = false;
-// //           });
-
-// //           break;
-
-// //         case FlutterBluetoothSerial.DISCONNECTED:
-// //           setState(() {
-// //             _connected = false;
-// //             _pressed = false;
-// //           });
-// //           break;
-
-// //         default:
-// //           print(state);
-// //           break;
-// //       }
-// //     });
-
-// //     // It is an error to call [setState] unless [mounted] is true.
-// //     if (!mounted) {
-// //       return;
-// //     }
-
-// //     // Store the [devices] list in the [_devicesList] for accessing
-// //     // the list outside this class
-// //     setState(() {
-// //       _devicesList = devices;
-// //     });
-// //   }
-// // /////////////////////////////////////
-
-// //   // Create the List of devices to be shown in Dropdown Menu
-// //   List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
-// //     List<DropdownMenuItem<BluetoothDevice>> items = [];
-// //     if (_devicesList.isEmpty) {
-// //       items.add(DropdownMenuItem(
-// //         child: Text('NONE'),
-// //       ));
-// //     } else {
-// //       _devicesList.forEach((device) {
-// //         items.add(DropdownMenuItem(
-// //           child: Text(device.name),
-// //           value: device,
-// //         ));
-// //       });
-// //     }
-// //     return items;
-// //   }
-
-// //   // Method to connect to bluetooth
-// //   void _connect() {
-// //     if (_device == null) {
-// //       show('No device selected');
-// //     } else {
-// //       bluetooth.isConnected.then((isConnected) {
-// //         if (!isConnected) {
-// //           bluetooth
-// //               .connect(_device)
-// //               .timeout(Duration(seconds: 60))
-// //               .catchError((error) {
-// //             setState(() => _pressed = false);
-// //           });
-// //           setState(() => _pressed = true);
-// //         }
-// //       });
-// //     }
-// //   }
-
-// //   // Method to disconnect bluetooth
-// //   void _disconnect() {
-// //     bluetooth.disconnect();
-// //     setState(() => _pressed = true);
-// //   }
-
-// //   // Method to show a Snackbar,
-// //   // taking message as the text
-// //   Future show(
-// //     String message, {
-// //     Duration duration: const Duration(seconds: 3),
-// //   }) async {
-// //     await new Future.delayed(new Duration(milliseconds: 100));
-// //     _scaffoldKey.currentState.showSnackBar(
-// //       new SnackBar(
-// //         content: new Text(
-// //           message,
-// //         ),
-// //         duration: duration,
-// //       ),
-// //     );
-// //   }
-
-// //   // Method to send message,
-// //   // for turning the bletooth device on
-// //   void _sendOnMessageToBluetooth() {
-// //     bluetooth.isConnected.then((isConnected) {
-// //       if (isConnected) {
-// //         bluetooth.write("1");
-// //         show('Device Turned On');
-// //       }
-// //     });
-// //   }
-
-// //   // Method to send message,
-// //   // for turning the bletooth device off
-// //   void _sendOffMessageToBluetooth() {
-// //     bluetooth.isConnected.then((isConnected) {
-// //       if (isConnected) {
-// //         bluetooth.write("0");
-// //         show('Device Turned Off');
-// //       }
-// //     });
-// //   }
-// // /////////////////////////////////////////////////
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return MaterialApp(
-// //       home: Scaffold(
-// //         key: _scaffoldKey,
-// //         appBar: AppBar(
-// //           title: Text("Flutter Bluetooth"),
-// //           backgroundColor: Colors.deepPurple,
-// //         ),
-// //         body: Container(
-// //           // Defining a Column containing FOUR main Widgets wrapped with some padding:
-// //           // 1. Text
-// //           // 2. Row
-// //           // 3. Card
-// //           // 4. Text (wrapped with "Expanded" and "Padding")
-// //           child: Column(
-// //             mainAxisSize: MainAxisSize.max,
-// //             children: <Widget>[
-// //               Padding(
-// //                 padding: const EdgeInsets.only(top: 8.0),
-// //                 child: Text(
-// //                   "PAIRED DEVICES",
-// //                   style: TextStyle(fontSize: 24, color: Colors.blue),
-// //                   textAlign: TextAlign.center,
-// //                 ),
-// //               ),
-// //               Padding(
-// //                 padding: const EdgeInsets.all(8.0),
-// //                 // Defining a Row containing THREE main Widgets:
-// //                 // 1. Text
-// //                 // 2. DropdownButton
-// //                 // 3. RaisedButton
-// //                 child: Row(
-// //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-// //                   children: <Widget>[
-// //                     Text(
-// //                       'Device:',
-// //                       style: TextStyle(
-// //                         fontWeight: FontWeight.bold,
-// //                       ),
-// //                     ),
-// //                     DropdownButton(
-// //                       // To be implemented : _getDeviceItems()
-// //                       items: _getDeviceItems(),
-// //                       onChanged: (value) => setState(() => _device = value),
-// //                       value: _device,
-// //                     ),
-// //                     RaisedButton(
-// //                       onPressed:
-// //                           // To be implemented : _disconnect and _connect
-// //                           _pressed
-// //                               ? null
-// //                               : _connected
-// //                                   ? _disconnect
-// //                                   : _connect,
-// //                       child: Text(_connected ? 'Disconnect' : 'Connect'),
-// //                     ),
-// //                   ],
-// //                 ),
-// //               ),
-// //               Padding(
-// //                 padding: const EdgeInsets.all(16.0),
-// //                 child: Card(
-// //                   elevation: 4,
-// //                   child: Padding(
-// //                     padding: const EdgeInsets.all(8.0),
-// //                     // Defining a Row containing THREE main Widgets:
-// //                     // 1. Text (wrapped with "Expanded")
-// //                     // 2. FlatButton
-// //                     // 3. FlatButton
-// //                     child: Row(
-// //                       children: <Widget>[
-// //                         Expanded(
-// //                           child: Text(
-// //                             "DEVICE 1",
-// //                             style: TextStyle(
-// //                               fontSize: 20,
-// //                               color: Colors.green,
-// //                             ),
-// //                           ),
-// //                         ),
-// //                         FlatButton(
-// //                           onPressed:
-// //                               // To be implemented : _sendOnMessageToBluetooth()
-// //                               _connected ? _sendOnMessageToBluetooth : null,
-// //                           child: Text("ON"),
-// //                         ),
-// //                         FlatButton(
-// //                           onPressed:
-// //                               // To be implemented : _sendOffMessageToBluetooth()
-// //                               _connected ? _sendOffMessageToBluetooth : null,
-// //                           child: Text("OFF"),
-// //                         ),
-// //                       ],
-// //                     ),
-// //                   ),
-// //                 ),
-// //               ),
-// //               Expanded(
-// //                 child: Padding(
-// //                   padding: const EdgeInsets.all(20),
-// //                   child: Center(
-// //                     child: Text(
-// //                       "NOTE: If you cannot find the device in the list, "
-// //                       "please turn on bluetooth and pair the device by "
-// //                       "going to the bluetooth settings",
-// //                       style: TextStyle(
-// //                           fontSize: 15,
-// //                           fontWeight: FontWeight.bold,
-// //                           color: Colors.red),
-// //                     ),
-// //                   ),
-// //                 ),
-// //               )
-// //             ],
-// //           ),
-// //         ),
-// //       ),
-// //     );
-// //   }
-// }
